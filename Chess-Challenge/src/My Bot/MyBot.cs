1c92f8001e59c055e36d39b5129271c290c09415
AlbinWilander <albwi477@student.liu.se>
2023-07-31 23:13:38 +0200

    Cleaned up the code for submission

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -12,7 +12,7 @@ public class MyBot : IChessBot
 {
     bool doOnce = true;
     int downUp = 0;
-    bool isWhite = false; //we don't know
+    bool isWhite = false;
 
     public Move Think(Board board, Timer timer)
     {
@@ -34,7 +34,6 @@ public class MyBot : IChessBot
 
         Move bestMove = new Move();
         float bestValue = float.MinValue;
-        //Dictionary<Move,int> rankedList = new Dictionary<Move, int>();
 
         foreach (var move in moves)
         {
@@ -43,52 +42,23 @@ public class MyBot : IChessBot
             value = board.GetPiece(move.StartSquare).IsKing ? value - 1.5f : value;
             value = board.GetPiece(move.StartSquare).IsPawn ? value+(move.TargetSquare.Rank - move.StartSquare.Rank) * downUp *0.25f: value;
             value = value + (move.TargetSquare.Rank - move.StartSquare.Rank) * downUp * 0.1f;
-            value = move.IsCapture ? getPieceValue(board.GetPiece(move.TargetSquare).PieceType) + value : value;
-            //board.TrySkipTurn();
-            //value = board.SquareIsAttackedByOpponent(move.TargetSquare) ? - getPieceValue(move.MovePieceType) + value : value; //risk of being captured
+
             value = board.SquareIsAttackedByOpponent(move.StartSquare)&&!board.SquareIsAttackedByOpponent(move.TargetSquare) ? getPieceValue(move.MovePieceType) + value : value; //move only if you can get to saftey
-#if DEBUG
-            
-            if (move.TargetSquare.File == 1)
-            {
 
-            }
-            //Console.WriteLine("move: " + move + " my value: " + getLowestValueInList(GetPiecesAttackingSquare(board, move.TargetSquare, false)) + " - " + "enemy value: " + getLowestValueInList(GetPiecesAttackingSquare(board, move.TargetSquare, true)));
-
-#endif
-            //board.UndoSkipTurn();
             value = ((float)getNumberOfSeenSquares(move.TargetSquare, move.MovePieceType, board) - (float)getNumberOfSeenSquares(move.StartSquare, move.MovePieceType, board))/4 + value; //prio lots of vision
             value = move.IsCastles ? value + 1 : value;
             value = move.IsEnPassant ? value + 2 : value;
             value = move.IsPromotion ? value + 10 : value;
             value = board.SquareIsAttackedByOpponent(move.TargetSquare) ? getLowestValueInList(GetPiecesAttackingSquare(board, move.TargetSquare, false)) - getLowestValueInList(GetPiecesAttackingSquare(board, move.TargetSquare, true)) + value: value;
             board.MakeMove(move);
-            //float test = getLowestValueInList(GetPiecesAttackingSquare(board, move.TargetSquare, false)) - getLowestValueInList(GetPiecesAttackingSquare(board, move.TargetSquare, true));
-            //if (test > 0)
-            //{
-            //    Console.WriteLine(board.GetFenString());
-            //    Console.WriteLine(test + "\n" + move);
-            //}
             value = board.IsInCheck()&&board.SquareIsAttackedByOpponent(move.TargetSquare) ? value - 10 : value;
             value = board.IsInCheck()? value + 5: value;
             value = board.IsInCheckmate() ? value + 1000 : value;
-#if DEBUG
-
-            if (value > 900)
-            {
-                Console.WriteLine("move:"+move+" checkmate?");
-            }
-#endif
 
             //Draw eval
             float diff = getBoardValueDiff(board, isWhite);
             value = board.IsDraw() ? value - diff : value;
-#if DEBUG
-            if(getBoardValueDiff(board, isWhite) < 0)
-            {
-                //Console.WriteLine("Want draw with: " + diff + " with move: " + move);
-            }
-#endif
+
             
 
             //board.ForceSkipTurn();
@@ -105,11 +75,6 @@ public class MyBot : IChessBot
             }
         }
 
-#if DEBUG
-        Console.WriteLine("val: " + bestValue + " move: " + bestMove);
-        Console.WriteLine("diff: "+getBoardValueDiff(board, isWhite));
-#endif
-        //BitboardHelper.GetKnightAttacks
         return new KeyValuePair<Move, float>(bestMove, bestValue);
     }
 
@@ -136,11 +101,6 @@ public class MyBot : IChessBot
         }
     }
 
-    /// <summary>
-    /// seems to work
-    /// </summary>
-    /// <param name="board"></param>
-    /// <returns></returns>
     public AttackDefendingBitboards getProtectedSquares(Board board)
     {
         AttackDefendingBitboards attackDefendingBitboards = new AttackDefendingBitboards();
@@ -164,32 +124,6 @@ public class MyBot : IChessBot
         }
         return attackDefendingBitboards;
     }
-    //public List<Piece> squareAttackedBy(Board board, Square square)
-    //{
-    //    Dictionary<Square, List<Piece>> squaresInPlay = new Dictionary<Square, List<Piece>>();
-    //    AttackDefendingBitboards attackDefendingBitboards = getProtectedSquares(board);
-    //    foreach (PieceList pieces in board.GetAllPieceLists())
-    //    {
-    //        foreach (Piece piece in pieces)
-    //        {
-
-    //            if (!(isWhite ^ piece.IsWhite)) //logical AND
-    //            {
-    //                if(BitboardHelper.SquareIsSet(attackDefendingBitboards.defendingSquares, square))
-    //                {
-    //                    // add square and all pieces attacking that square squaresInPlay.Add(square,new List<Piece>());
-
-    //                }
-    //            }
-    //            else if (isWhite ^ piece.IsWhite) //logical XOR (no need for else if)
-    //            {
-    //                squaresInPlay.Add(BitboardHelper.SquareIsSet(attackDefendingBitboards.attackedSquares, square));
-
-    //            }
-    //        }
-    //    }
-
-    //}
 
     public HashSet<PieceType> GetPiecesAttackingSquare(Board board, Square square, bool my)
     {
@@ -218,9 +152,6 @@ public class MyBot : IChessBot
             }
         }
 
-#if DEBUG
-        //BitboardHelper.VisualizeBitboard(attackDefendingBitboards._attackedSquares[PieceType.Bishop]);
-#endif
 
         return attackingPieces;
     }
